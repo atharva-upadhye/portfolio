b7a4447cb24e776f1cf868166c9ac1c81f84c631
Atharva Upadhye <70993547+atharva-upadhye@users.noreply.github.com>
2025-09-08 00:55:22 +0530

    add accessibility skip to content w3c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,53 +9,59 @@ import { ThemeProvider } from "next-themes";
 // import { scriptFn as initScript } from "@/utils/script";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio - Atharva Upadhye",
-  description:
-    "Atharva Upadhye | Full Stack Developer proficient in MERN(MongoDb, Express, React, Node) stack, Typescript and other technologies.",
+	title: "Portfolio - Atharva Upadhye",
+	description:
+		"Atharva Upadhye | Full Stack Developer proficient in MERN(MongoDb, Express, React, Node) stack, Typescript and other technologies.",
 };
 
 export default function RootLayout({
-  children,
-  projects,
-  timeline,
-  social,
-  skills,
-  blogs,
+	children,
+	projects,
+	timeline,
+	social,
+	skills,
+	blogs,
 }: LayoutProps) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(geistSans.variable, geistMono.variable, "antialiased")}
-      >
-        {/* <script
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={cn(geistSans.variable, geistMono.variable, "antialiased")}
+			>
+				<a id="skip-to-content" href="#content">
+					Skip to Content
+				</a>
+				{/* <script
           suppressHydrationWarning
           dangerouslySetInnerHTML={{ __html: `(${initScript.toString()})()` }}
         /> */}
-        <ThemeProvider enableSystem attribute={"class"}>
-          <ThemeSwitcher />
-          <ScrollToTop />
-          <main className="m-auto max-w-4xl p-8 flex flex-col gap-4">
-            {children}
-            {projects}
-            {skills}
-            {social}
-            {/* {certifications} */}
-            {blogs}
-            {timeline}
-          </main>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+				<ThemeProvider enableSystem attribute={"class"}>
+					<ThemeSwitcher />
+					<ScrollToTop />
+					<main
+						id="content"
+						className="m-auto max-w-4xl p-8 flex flex-col gap-4"
+					>
+						{children}
+						{projects}
+						{skills}
+						{social}
+						{/* {certifications} */}
+						{blogs}
+						{timeline}
+					</main>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
