d0be5f6b4102fe438fb9d661f50a8a7711d5e5df
Atharva Upadhye <70993547+atharva-upadhye@users.noreply.github.com>
2025-09-08 01:06:58 +0530

    add accessibility a11y eslint plugin

--- src/components/scroll-to-top.tsx
+++ src/components/scroll-to-top.tsx
@@ -5,24 +5,24 @@ import { IconArrowUpCircle } from "./icons";
 import { twColor, defaultClassName } from "@/utils/tailwind";
 
 export default function ScrollToTop() {
-  const { show } = useShowScrollToTop();
-  return (
-    <div
-      className={`flex flex-row fixed bottom-0 right-0 ${
-        show ? "" : "translate-y-full"
-      } transition-all`}
-    >
-      <div
-        className={`${twColor.button.light.bg} ${twColor.button.dark.bg} m-3 p-2 rounded-md`}
-      >
-        <a
-          href="#"
-          title="scroll to top"
-          className={`rounded-md p-2 ${defaultClassName.button()} `}
-        >
-          <IconArrowUpCircle />
-        </a>
-      </div>
-    </div>
-  );
+	const { show } = useShowScrollToTop();
+	return (
+		<div
+			className={`flex flex-row fixed bottom-0 right-0 ${
+				show ? "" : "translate-y-full"
+			} transition-all`}
+		>
+			<div
+				className={`${twColor.button.light.bg} ${twColor.button.dark.bg} m-3 p-2 rounded-md`}
+			>
+				<a
+					href="#content"
+					title="scroll to top"
+					className={`rounded-md p-2 ${defaultClassName.button()} `}
+				>
+					<IconArrowUpCircle />
+				</a>
+			</div>
+		</div>
+	);
 }
